49d0536d90064864a9d6c2d252d0bf959b49584a
Abadie Mylène <mylene.abadie@hotmail.fr>
2024-02-26 20:33:44 +0100

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Baby Birth Games",
+  description: "Send games for your baby shower",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/images/biberon.png',
+        href: '/images/biberon.png',
+      }
+    ]
+  }
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
