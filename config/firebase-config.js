8fc88d38df214019c9b36fa85b352008708ecc59
Abadie Mylène <mylene.abadie@hotmail.fr>
2024-02-28 21:12:23 +0100

    ajout des outils pour deployer sur Firebase

---
+++ config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRLK9RnnjzOS8P3TtkS36Jcw88SasL6-E",
+  authDomain: "guess-my-name-638d7.firebaseapp.com",
+  projectId: "guess-my-name-638d7",
+  storageBucket: "guess-my-name-638d7.appspot.com",
+  messagingSenderId: "755872974919",
+  appId: "1:755872974919:web:14d224415259600d634962"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
